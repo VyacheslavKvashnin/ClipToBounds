963629478c0b9c9bee7a69726b931df509e4adc7
komplektan <komplektan@mail.ru>
2022-07-31 22:21:24 +0500

    End

--- ClipToBounds/ViewController.swift
+++ ClipToBounds/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let view1: UIControl = {
+       let view = UIControl()
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        view.backgroundColor = .red
+        view.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        return view
+    }()
+    
+    let view2: UIControl = {
+       let view = UIControl()
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        view.backgroundColor = .blue
+        view.addTarget(self, action: #selector(tapButton2), for: .touchUpInside)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+        
+        view.addSubview(view1)
+        view1.frame.origin = CGPoint(x: 100, y: 100)
+        view2.frame.origin = CGPoint(x: 150, y: 150)
+        view.addSubview(view2)
+        
     }
-
-
+    
+    @objc func tapButton() {
+        print("Tap 1")
+    }
+    @objc func tapButton2() {
+        print("Tap 2")
+    }
 }
 
+
+extension UIControl {
+    open override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let expendedBounds = self.bounds.insetBy(dx: -50, dy: -50)
+        return expendedBounds.contains(point)
+    }
+}
